a916a8dea2c0b514ec459330e7103cac6e12c8c3
roolio <julien.laugel@mfglabs.com>
2013-08-28 17:48:33 +0200

    debug scala file

--- src/main/g8/src/main/scala/$group_id__packaged$/$main_class$.scala
+++ src/main/g8/src/main/scala/$group_id__packaged$/$main_class$.scala
@@ -22,5 +22,5 @@ class MyRegistrator extends spark.KryoRegistrator {
 object $main_class$ extends App {
 	System.setProperty("spark.serializer", "spark.KryoSerializer")	//better but not mandatory
 	System.setProperty("spark.executor.memory", "12g")	//change if needed
-	val sc = new SparkContext("spark://INSERT_YOUR_IP:7077", "$name$","/home/hadoop/spark/", List($name$-$version$-shaded.jar))
+	val sc = new SparkContext("spark://INSERT_YOUR_IP:7077", "$name$","/home/hadoop/spark/", List("$name$-$version$-shaded.jar"))
 }
